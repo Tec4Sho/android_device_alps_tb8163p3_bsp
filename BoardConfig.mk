ea81951c93be5b442ab9574a6d9428b1858923d7
Ziona <143420949+Tec4Sho@users.noreply.github.com>
2025-09-11 19:57:58 -0400

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -378,3 +378,5 @@ TW_HAS_NO_RECOVERY_PARTITION := false
 # VINTF
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 #DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/vendor-manifest.xml
+
+# Auto copy files into ramdisk-recovery

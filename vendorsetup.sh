90a3812a0202d403abdd831e842f3c614c9ec2ab
Ziona <143420949+Tec4Sho@users.noreply.github.com>
2025-09-14 04:33:48 -0400

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -111,7 +111,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
    export OF_RUN_POST_FORMAT_PROCESS=1
 
  # fux screen height for tablets
-   export OF_SCREEN_H="720"
+   export OF_SCREEN_H="1080"
  
  # Maintainer 
    export OF_MAINTAINER="Tec4Sho"

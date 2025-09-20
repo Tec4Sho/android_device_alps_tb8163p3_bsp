ec131ae1ebbbd1bc996cd7c6f4c6f4e81e3624ec
Ziona <143420949+Tec4Sho@users.noreply.github.com>
2025-09-19 21:33:43 -0400

    Update omni_tb8163p3_bsp.mk

--- omni_tb8163p3_bsp.mk
+++ omni_tb8163p3_bsp.mk
@@ -9,8 +9,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-# $(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/pb/config/common.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
+# $(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit from tb8163p3_bsp device
 $(call inherit-product, device/alps/tb8163p3_bsp/device.mk)

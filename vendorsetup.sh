d0dbeb0eb91fa91198ded396a41020d8ce96d7f3
Ziona <143420949+Tec4Sho@users.noreply.github.com>
2025-10-17 16:40:05 -0400

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
 #	 This file is part of the OrangeFox Recovery Project
 # 	 Copyright (C) 2021-2024 The OrangeFox Recovery Project
 #	 Copyright (C) 2024 The Android Open Source Project

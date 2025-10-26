a7880a258e951c23f838cba61fc36ee519757407
Ziona <143420949+Tec4Sho@users.noreply.github.com>
2025-10-26 15:34:20 -0400

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -121,7 +121,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
    export OF_RUN_POST_FORMAT_PROCESS=1
 
  # fux screen height for tablets
-   export OF_SCREEN_H="600";;
+   export OF_SCREEN_H="600"
  
  # Maintainer 
    export OF_MAINTAINER="Tec4Sho"
@@ -134,7 +134,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
       export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
       export | grep "TW_" >> $FOX_BUILD_LOG_FILE
    fi
-fi
+ fi
 fi
 
  add_lunch_combo omni_tb8163p3_bsp-user

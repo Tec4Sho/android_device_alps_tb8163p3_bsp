a39d7f0aac95f484d3631f5b91835437a9facdbd
Ziona <143420949+Tec4Sho@users.noreply.github.com>
2026-03-20 18:50:34 -0400

    Update device_hooks.mk

--- device_hooks.mk
+++ device_hooks.mk
@@ -5,9 +5,11 @@ SHELL := /bin/bash
 MY_MKIMAGE := /home/runner/work/android_device_alps_tb8163p3_bsp/android_device_alps_tb8163p3_bsp/workspace/$(TARGET_KERNEL_SOURCE)/scripts/mkimage
 MTK_KERNEL_CFG := /home/runner/work/android_device_alps_tb8163p3_bsp/android_device_alps_tb8163p3_bsp/workspace/$(DEVICE_PATH)/mtk_kernel.cfg
 # Update K_TARGET to point to the actual binary file
-K_TARGET := $(PRODUCT_OUT)/obj/KERNEL_OBJ/arch/arm/boot/zImage
+# K_TARGET := $(PRODUCT_OUT)/obj/KERNEL_OBJ/arch/arm/boot/zImage
 # If it's a 64-bit build, it might be:
 # K_TARGET := $(PRODUCT_OUT)/obj/KERNEL_OBJ/arch/arm64/boot/Image.gz-dtb
+# Patch the file that actually gets packed into the image
+K_TARGET := $(PRODUCT_OUT)/kernel
 
 # Define the path to your new script
 PATCH_SCRIPT := $(DEVICE_PATH)/patch_kernel.sh

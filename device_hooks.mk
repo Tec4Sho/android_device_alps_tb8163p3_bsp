a2b6a0372d3a0daee1761ce1b9f326bd5ae8ce98
Ziona <143420949+Tec4Sho@users.noreply.github.com>
2026-03-20 14:32:36 -0400

    Update device_hooks.mk

--- device_hooks.mk
+++ device_hooks.mk
@@ -4,7 +4,7 @@
 MY_MKIMAGE := $(TARGET_KERNEL_SOURCE)/scripts/mkimage
 MTK_KERNEL_CFG := $(DEVICE_PATH)/mtk_kernel.cfg
 DYN_OUT := $(get_build_var OUT_DIR)
-K_SRC := /home/runner/work/android_device_alps_tb8163p3_bsp/android_device_alps_tb8163p3_bsp/workspace/out/target/product/tb8163p3_bsp/obj/KERNEL_OBJ/kernel
+K_TARGET := /home/runner/work/android_device_alps_tb8163p3_bsp/android_device_alps_tb8163p3_bsp/workspace/out/target/product/tb8163p3_bsp/obj/KERNEL_OBJ/kernel
 
 # Intercept the recovery image target
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(recovery_kernel) $(MTK_KERNEL_CFG)
@@ -14,11 +14,11 @@ $(INSTALLED_RECOVERYIMAGE_TARGET): $(recovery_kernel) $(MTK_KERNEL_CFG)
 	# 2. Run the patch: tool + input + cfg > output
 	# Use quotes everywhere to prevent "binary operator" errors from long paths
     @if [[ -s "$(recovery_kernel)" ]]; then \
-	    "$(MY_MKIMAGE)" "$(recovery_kernel)" "$(MTK_KERNEL_CFG)" > "$(recovery_kernel).mtk" \
+	    "$(MY_MKIMAGE)" "$(recovery_kernel)" "$(MTK_KERNEL_CFG)" > "$(recovery_kernel).mtk"; \
         echo "--- MTK Kernel Patching Stage Complete! $(recovery_kernel) ---"; \
     else \
-        "$(MY_MKIMAGE)" "$(K_SRC)" "$(MTK_KERNEL_CFG)" > "$(K_SRC).mtk" \
-        echo "--- MTK Kernel Patching Stage Complete! $(K_SRC) ---"; \
+        "$(MY_MKIMAGE)" "$(K_TARGET)" "$(MTK_KERNEL_CFG)" > "$(K_TARGET).mtk"; \
+        echo "--- MTK Kernel Patching Stage Complete! $(K_TARGET) ---"; \
     fi;
 	# 3. Use standard bash [[ ]] with -s (checks if file exists and is not empty)
 	@if [[ -s "$(recovery_kernel).mtk" ]]; then \
@@ -26,12 +26,11 @@ $(INSTALLED_RECOVERYIMAGE_TARGET): $(recovery_kernel) $(MTK_KERNEL_CFG)
 		echo "--- SUCCESS: MTK Header added to $(recovery_kernel) ---"; \
         exit 0; \
 	else \
-		echo "--- ERROR: $(K_SRC).mtk is missing or empty! ---"; \
+		echo "--- ERROR: $(recovery_kernel).mtk is missing or empty! ---"; \
 	fi;
-    @if [[ -s "$(K_SRC).mtk" ]]; then \
-		mv -f "$(K_SRC).mtk" "$(K_SRC)"; \
-		echo "--- SUCCESS: MTK Header added to $(K_SRC) ---"; \
-        echo "--- MTK Kernel Patching Stage Complete! $(DYN_OUT) ---"; \
+    @if [[ -s "$(K_TARGET).mtk" ]]; then \
+		mv -f "$(K_TARGET).mtk" "$(K_TARGET)"; \
+		echo "--- SUCCESS: MTK Header added to $(K_TARGET) ---"; \
 	else \
-		echo "--- ERROR: $(K_SRC).mtk is missing or empty! ---"; \
+		echo "--- ERROR: $(K_TARGET).mtk is missing or empty! ---"; \
 	fi;

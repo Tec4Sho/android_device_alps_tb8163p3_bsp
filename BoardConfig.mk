2f189216e0632b709eba0e21444b889999ec83f0
Ziona <143420949+Tec4Sho@users.noreply.github.com>
2026-03-23 15:12:10 -0400

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -431,7 +431,7 @@ BOARD_USES_MTK_HEADER := true
 BOARD_USES_MTK_KERNEL_HEADER := true
 BOARD_BOOTIMG_HEADER_VERSION := 1
 BOARD_KERNEL_BASE := 0x40000000
-BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,32N2 androidboot.selinux=permissive androidboot.init_fatal_reboot_target=recovery console=ttyS0,921600n1 root=/dev/ram androidboot.hardware=mt8163 firmware_class.path=/vendor/firmware build=06_12_24(gA1A470FC) lcm_id=4000 brightness=8388613 avdd=52 vcom=46 skip_initramfs ro rootwait init=/init root=PARTUUID=e7099731-95a6-45a6-a1e5-1b6aba032cf1 androidboot.verifiedbootstate=orange androidboot.atm=disabled androidboot.meta_log_disable=0 androidboot.dtbo_idx=0 printk.disable_uart=0 bootprof.pl_t=1680 bootprof.lk_t=7245 boot_reason=4 androidboot.serialno=SC5SKNLBEYIRBMR4 androidboot.bootreason=reboot mrdump_ddrsv=yes mrdump_rsvmem=0x56000000,0x400000,0x42000000,0x17f740,0x54000000,0x80000
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,32N2 androidboot.selinux=permissive printk.devkmsg=on androidboot.init_fatal_reboot_target=recovery console=ttyS0,921600n1 root=/dev/ram androidboot.hardware=mt8163 firmware_class.path=/vendor/firmware build=06_12_24(gA1A470FC) lcm_id=4000 brightness=8388613 avdd=52 vcom=46 skip_initramfs ro rootwait init=/init root=PARTUUID=e7099731-95a6-45a6-a1e5-1b6aba032cf1 androidboot.verifiedbootstate=orange androidboot.atm=disabled androidboot.meta_log_disable=0 androidboot.dtbo_idx=0 printk.disable_uart=0 bootprof.pl_t=1680 bootprof.lk_t=7245 boot_reason=4 androidboot.serialno=SC5SKNLBEYIRBMR4 androidboot.bootreason=reboot mrdump_ddrsv=yes mrdump_rsvmem=0x56000000,0x400000,0x42000000,0x17f740,0x54000000,0x80000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_DTBO_SIZE := 40393
 BOARD_RAMDISK_OFFSET := 0x15000000
@@ -485,16 +485,5 @@ TARGET_COPY_OUT_VENDOR := vendor
 # Define the path to the ramdisk that gets packed into recovery
 RECOVERY_RAMDISK_TARGET := $(PRODUCT_OUT)/ramdisk-recovery.img
 MTK_RAMDISK_CFG := $(DEVICE_PATH)/mtk_ramdisk.cfg
-
-# This macro runs immediately after the ramdisk is created
-define add-mtk-header-to-ramdisk
-	@echo "--- Adding MTK ROOTFS Header to $(RECOVERY_RAMDISK_TARGET) ---"
-	$(hide) $(MKIMAGE) $(RECOVERY_RAMDISK_TARGET) $(MTK_RAMDISK_CFG) > $(RECOVERY_RAMDISK_TARGET).mtk
-	$(hide) mv -f $(RECOVERY_RAMDISK_TARGET).mtk $(RECOVERY_RAMDISK_TARGET)
-endef
-
-# Hook the macro into the recovery image creation process
-$(INSTALLED_RECOVERYIMAGE_TARGET): $(RECOVERY_RAMDISK_TARGET)
-	$(add-mtk-header-to-ramdisk)
 
 

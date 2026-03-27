b4d5b239ded75362c2ecf68cee99ba24c2995f47
Ziona <143420949+Tec4Sho@users.noreply.github.com>
2026-03-27 00:09:43 -0400

    Update omni_tb8163p3_bsp.mk

--- omni_tb8163p3_bsp.mk
+++ omni_tb8163p3_bsp.mk
@@ -10,10 +10,10 @@
 
 # Add this line if your device is 64-bit
 # Otherwise, If you have 32-bit device, add the below line instead of above line
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
+# $(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
 
 # Another common config inclusion
-# $(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product-if-exists, vendor/omni/config/common.mk)

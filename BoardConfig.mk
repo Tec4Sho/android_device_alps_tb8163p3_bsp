de6a3bebc750930a848464293308b04b29b1691a
Ziona <143420949+Tec4Sho@users.noreply.github.com>
2026-03-27 20:16:03 -0400

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -175,18 +175,18 @@ else ifeq ($(RECOVERY_VARIANT),pbrp)
   PB_BUILD_TYPE := UNOFFICIAL
   TW_OEM_BUILD := true
   TW_THEME := portrait_hdpi
-  #TW_CUSTOM_THEME := $(DEVICE_PATH)/pbrp/twres
-  #TWRP_THEME_LOC := $(TW_CUSTOM_THEME)
-  #TARGET_RECOVERY_DEVICE_DIRS += $(DEVICE_PATH)
-  #TWRP_NEW_THEME := false
+  # TW_CUSTOM_THEME := $(DEVICE_PATH)/pbrp/twres
+  # TWRP_THEME_LOC := $(TW_CUSTOM_THEME)
+  # TARGET_RECOVERY_DEVICE_DIRS += $(DEVICE_PATH)
+  TWRP_NEW_THEME := false
   RECOVERY_VARIANT := pbrp
   # VNDK Fix
   BOARD_VNDK_VERSION := current
-  BOARD_VNDK_RUNTIME_DISABLE := true
+  BOARD_VNDK_RUNTIME_DISABLE := false
   # Uses Custom ARM Busybox w/ Toybox
   # RECOVERY_BUSYBOX_SYMLINKS := false
   # RECOVERY_BUSYBOX_TOOLS := false
-  #DEVICE_RESOLUTION := 1080x600  # The Resolution of your Device
+  # DEVICE_RESOLUTION := 1080x600  # The Resolution of your Device
   BOARD_SCREEN_WIDTH := 720       # 600 True width mdpi
   BOARD_SCREEN_HEIGHT := 1280     # 1024 True height mdpi
   DEVICE_SCREEN_WIDTH := 720      # Device resolution width
@@ -205,8 +205,8 @@ else ifeq ($(RECOVERY_VARIANT),pbrp)
   TARGET_RECOVERY_OVERSCAN_PERCENT := 0
   TW_X_OFFSET := 0
   TW_Y_OFFSET := 0
-  #TW_W_OFFSET := 0
-  #TW_H_OFFSET := 0
+  # TW_W_OFFSET := 0
+  # TW_H_OFFSET := 0
 else ifeq ($(RECOVERY_VARIANT),shrp)
   TW_OEM_BUILD := false
   TW_THEME := portrait_hdpi

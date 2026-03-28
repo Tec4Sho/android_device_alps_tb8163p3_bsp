ee206dac8121a39133743e57419ded6317a6884d
Ziona <143420949+Tec4Sho@users.noreply.github.com>
2026-03-28 19:04:34 -0400

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -248,7 +248,7 @@ else
   # Uses Custom ARM Busybox w/ Toybox
   #RECOVERY_BUSYBOX_SYMLINKS := true
   #RECOVERY_BUSYBOX_TOOLS := true
-  #DEVICE_RESOLUTION := 1080x600                 # The Resolution of your Device
+  DEVICE_RESOLUTION := 1024x600                 # The Resolution of your Device
   #BOARD_SCREEN_WIDTH := 1280                     # Device resolution width
   #BOARD_SCREEN_HEIGHT := 720                     # Device resolution height
   #TARGET_SCREEN_HEIGHT := 1024                    # The height mdpi

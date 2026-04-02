9e63f48beea53b2719b06759e4905b3e00a1802f
Ziona <143420949+Tec4Sho@users.noreply.github.com>
2026-04-02 16:48:54 -0400

    Update omni_tb8163p3_bsp.mk

--- omni_tb8163p3_bsp.mk
+++ omni_tb8163p3_bsp.mk
@@ -4,14 +4,14 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-RECOVERY_VARIANT ?= pbrp
+# RECOVERY_VARIANT ?= pbrp
 
 # Inherit from those products. Most specific first.
 # $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/full_base.mk)
 
 # Add this line if your device is 64-bit
 # Otherwise, If you have 32-bit device, add the below line instead of above line
-# $(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
 
 # Another common config inclusion
 # $(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)

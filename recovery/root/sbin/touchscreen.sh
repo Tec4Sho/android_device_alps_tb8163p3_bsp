04bab6e0069186c68c04ac7fc015d70ced9fffe7
Ziona <143420949+Tec4Sho@users.noreply.github.com>
2026-04-21 00:35:19 -0400

    Enhance touchscreen.sh with bind mount logging
    Add logging for bind mount status of virtualkeys to sys properties.

--- recovery/root/sbin/touchscreen.sh
+++ recovery/root/sbin/touchscreen.sh
@@ -1,17 +1,17 @@
 #!/sbin/sh
 
 mount /apd >/dev/null 2>&1
+mount /cache >/dev/null 2>&1
+mkdir -p /cache/recovery >/dev/null 2>&1
 
 if mount | grep -qsF '/apd'; then
     [ -s /apd/virtualkeys ] && paste -sd ":" /apd/virtualkeys > /sbin/virtualkeys
-    mount --bind /sbin/virtualkeys /sys/board_properties/virtualkeys.TS_GT9xx
+    mount --bind /sbin/virtualkeys /sys/board_properties/virtualkeys.TS_GT9xx && echo -e "$(date)\nTOUCH STATUS: apd/virtualkeys was bind mounted to sys/board_properties/virtualkeys.TS_GT9xx" >/cache/recovery/recovery_touch_calibration >/dev/null 2>&1
 else
-    mount --bind /sbin/virtualkeys /sys/board_properties/virtualkeys.TS_GT9xx
+    mount --bind /sbin/virtualkeys /sys/board_properties/virtualkeys.TS_GT9xx && echo -e "$(date)\nTOUCH STATUS: sbin/virtualkeys was bind mounted to sys/board_properties/virtualkeys.TS_GT9xx" >/cache/recovery/recovery_touch_calibration >/dev/null 2>&1
 fi;
     # Set permissions so the touch driver can read it
     chmod 0444 /sys/board_properties/virtualkeys.TS_GT9xx
     chown root root /sys/board_properties/virtualkeys.TS_GT9xx
     setprop persist.sys.touch.config 1
-    mkdir -p /cache/recovery >/dev/null 2>&1
-    echo -e "$(date)\nSTATUS: virtualkeys mounted to virtualkeys.TS_GT9xx" >/cache/recovery/recovery_touch_calibration >/dev/null 2>&1
     

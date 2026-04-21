51c656bd85b768bd4985e75f3df930463ba4fd03
Ziona <143420949+Tec4Sho@users.noreply.github.com>
2026-04-21 00:18:49 -0400

    Update touchscreen.sh to suppress output
    Redirect output to /dev/null for mkdir and echo commands.

--- recovery/root/sbin/touchscreen.sh
+++ recovery/root/sbin/touchscreen.sh
@@ -12,6 +12,6 @@ fi;
     chmod 0444 /sys/board_properties/virtualkeys.TS_GT9xx
     chown root root /sys/board_properties/virtualkeys.TS_GT9xx
     setprop persist.sys.touch.config 1
-    echo -e "$(date)\n virtualkeys mounted to virtualkeys.TS_GT9xx" >/cache/recovery_touch_calibration
-    echo -e "$(date)\n virtualkeys mounted to virtualkeys.TS_GT9xx" >/data/media/0/recovery_touch_calibration
-
+    mkdir -p /cache/recovery >/dev/null 2>&1
+    echo -e "$(date)\n virtualkeys mounted to virtualkeys.TS_GT9xx" >/cache/recovery/recovery_touch_calibration >/dev/null 2>&1
+    

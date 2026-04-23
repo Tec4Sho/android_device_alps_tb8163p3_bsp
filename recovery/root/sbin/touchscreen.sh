bdc6a985fe8133d74668dc09000dfabc49f22172
Ziona <143420949+Tec4Sho@users.noreply.github.com>
2026-04-23 00:08:51 -0400

    Update touchscreen script to handle calibration file
    Copy recovery touch calibration file to data media and set permissions.

--- recovery/root/sbin/touchscreen.sh
+++ recovery/root/sbin/touchscreen.sh
@@ -3,6 +3,7 @@
 # Read/Write mount default partitions
     mount -o rw,remount /vendor || mount /vendor 2>/dev/null
     mount -o rw,remount /cache || mount /cache 2>/dev/null
+    mount -o rw,remount /data || mount /data 2>/dev/null
     mount -o rw,remount /apd || mount /apd 2>/dev/null
     mkdir -p /cache/touch 2>/dev/null
 
@@ -15,6 +16,10 @@ else
     mount --bind /sbin/virtualkeys /sys/board_properties/virtualkeys.TS_GT9xx && \
     echo -e "$(date)\nTOUCH STATUS: sbin/virtualkeys was bind mounted to sys/board_properties/virtualkeys.TS_GT9xx" > /cache/touch/recovery_touch_calibration 2>/dev/null;
 fi;
+    cp -f /cache/touch/recovery_touch_calibration /data/media/0/
+    chown 1023:1023 /data/media/0/recovery_touch_calibration
+    chmod 664 /data/media/0/recovery_touch_calibration
+    umount -l /data 2>/dev/null;
     # Set permissions so the touch driver can read it
     chmod 0444 /sys/board_properties/virtualkeys.TS_GT9xx
     chown root root /sys/board_properties/virtualkeys.TS_GT9xx
